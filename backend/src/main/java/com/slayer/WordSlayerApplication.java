0de85792839adb1a64440d295cede777ffdedd73
John Clarrence Reyes <jcreyes078@gmail.com>
2026-04-29 02:41:18 +0800

    file structure fixed

---
+++ backend/src/main/java/com/slayer/WordSlayerApplication.java
@@ -0,0 +1,11 @@
+package com.slayer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordSlayerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WordSlayerApplication.class, args);
+    }
+}
